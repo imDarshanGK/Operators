de1f512fdc4e41411d8594959989448e3d68e756
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-01-28 18:11:20 +0530

    logical operators
    logical operators

---
+++ logical operators.py
@@ -0,0 +1,14 @@
+#logical operators
+#and,or,not
+a=10
+b=20
+print(a<b and a!=b)
+print(a<b and a==b)
+print(a>b and a!=b)
+print(a>b and a==b)
+print(a<b or a!=b)
+print(a<b or a==b)
+print(a>b or a!=b)
+print(a>b or a==b)
+print(not a<b)
+print(not a>b)
