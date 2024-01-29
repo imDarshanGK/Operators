5fc141385cb084ed4e4e3912d667455617b83769
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-01-29 21:01:13 +0530

    Assignment Operators
    Assignment Operators

---
+++ Assignment Operators.py
@@ -0,0 +1,18 @@
+#Assignment Operators
+#symbol =,+=,-=,*=,/=,%=,//=
+
+a=20
+a=40
+print('a=',a)
+a+=40
+print('a+=',a)
+a-=40
+print('a-=',a)
+a*=40
+print('a*=',a)
+a/=40
+print('a/=',a)
+a%=40
+print('a%=',a)
+a//=40
+print('a//=',a)
