9fc7cc6b64e3f40f407b5dfd74ca164f5496b444
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-02-03 05:33:35 +0530

    Arithmetic operators
    Arithmetic operators

---
+++ Arithmetic operators/Arithmetic operators.py
@@ -0,0 +1,19 @@
+print("Arithmetic operators\n")
+
+# Addition + 
+# Substration -
+# Multiplication *
+# Division /
+# Modulus %
+# Exponentiation **
+# floor divison //
+
+a = 45
+b = 32
+print("Add =",a+b)
+print("Sub =",a-b)
+print("Mul =",a*b)
+print("Div =",a/b)
+print("Mod =",a%b)
+print("Exp =",a**b)
+print("F.D =",a//b)
