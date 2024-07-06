b12384cc849a9156a76d656f5fe46161e45ddfa3
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-07-06 22:51:06 +0530

    MS operators

---
+++ Membership Operators/Membership Operators.py
@@ -0,0 +1,11 @@
+alp = "Python program"
+print('p' in alp)
+print('m' in alp)
+print('z' not in alp)
+print('x' in alp)
+
+#Number
+num = [1,2,3,5,-1,-2]
+print(3 in num)
+print(-2 in num)
+print(55 in num)
