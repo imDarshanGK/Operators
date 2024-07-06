b67df73a10f8f4bfb34ab82e7601019c6f1468b0
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-07-06 22:53:22 +0530

    Membership Operators.py

--- Membership Operators/Membership Operators.py
+++ Membership Operators/Membership Operators.py
@@ -1,11 +1,11 @@
 alp = "Python program"
-print('p' in alp)
-print('m' in alp)
-print('z' not in alp)
-print('x' in alp)
+print('p' in alp)  #True
+print('m' in alp)  #True
+print('z' not in alp)  #True
+print('x' in alp)  #False
 
 #Number
 num = [1,2,3,5,-1,-2]
-print(3 in num)
-print(-2 in num)
-print(55 in num)
+print(3 in num)  #True
+print(-2 in num)  #True
+print(55 in num)  #False
